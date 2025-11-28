59c7c18215d9f47c2bd4f516fb14079bbca37803
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2025-11-27 23:36:04 -0300

    Add files via upload

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,36 +3,69 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    public Transform[] spawnPoints;
+    public Transform[] playersSpawnPoints;
+    public Transform[] fruitSpawnPoints;
     public GameObject playerPrefab;
     public GameObject[] ghostsPrefabs;
-    private int posPlayer;
+    public GameObject fruitPrefab;
+    private int playerPos;
     private int ghostCount = 0;
+    private int randFruit;
+    private bool repeatedFruit;
+    public int[] fruitPos = new int[5];
 
     void Start()
     {
 
         SpawnPlayers();
+        SpawnFruits();
         
     }
 
     void SpawnPlayers(){
 
-        posPlayer = Random.Range(0,4);
+        playerPos = Random.Range(0,4);
 
-        for(int i=0; i < spawnPoints.Length; i++){
+        for(int i=0; i < playersSpawnPoints.Length; i++){
 
-            if(i != posPlayer){
+            if(i != playerPos){
 
-                Instantiate(ghostsPrefabs[ghostCount], spawnPoints[i]);
+                Instantiate(ghostsPrefabs[ghostCount], playersSpawnPoints[i]);
                 ghostCount++;
 
             }else{
 
-                Instantiate(playerPrefab, spawnPoints[i]);
+                Instantiate(playerPrefab, playersSpawnPoints[i]);
 
             }
 
+        }
+
+    }
+
+    void SpawnFruits(){
+
+        for (int i = 0; i < fruitPos.Length; i++)
+        {
+
+            do{            
+                
+                repeatedFruit = false;
+                randFruit = Random.Range(0, 10);
+
+                for (int j=0; j < i; j++)
+                {
+                    if (fruitPos[j] == randFruit)
+                    {
+                        repeatedFruit = true;
+                        break;
+                    }
+                }
+
+            } while (repeatedFruit);
+
+            fruitPos[i] = randFruit;
+            Instantiate(fruitPrefab, fruitSpawnPoints[randFruit]);
         }
 
     }
