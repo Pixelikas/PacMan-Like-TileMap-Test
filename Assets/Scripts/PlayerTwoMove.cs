a8669cecd607f0d01e9725e4e36c65f8989e1e11
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2025-11-27 23:35:16 -0300

    Add files via upload

--- Assets/Scripts/PlayerTwoMove.cs
+++ Assets/Scripts/PlayerTwoMove.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class PlayerTwoMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Variables to horizontal and vertical input
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Variable to player speed
+    private float moveSpeed = 5f;
+
+    // Variable to player physics component (Rigidbody2D)
+    private Rigidbody2D playerPhysics;
+
     void Start()
+    {        
+
+        // Component reference
+        playerPhysics = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
     {
+
+        // Axis inputs to variables
+        horizontalInput = Input.GetAxis("HorizontalP2");
+        verticalInput = Input.GetAxis("VerticalP2");
+
+        // Calculate inputs x speed in new vector2 and use it to set linear velocity of player physics 
+        playerPhysics.linearVelocity = new Vector2(horizontalInput * moveSpeed, verticalInput * moveSpeed);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
